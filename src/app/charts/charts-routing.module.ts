3000e2f6358308cd06932fa2bce8b8b5c49d0c77
JonnyLai <m10573727@mail.nknu.edu.tw>
2017-05-23 15:28:44 +0800

    Sub Module Sample

---
+++ src/app/charts/charts-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FlotComponent } from "app/charts/flot/flot.component";
+
+const routes: Routes = [
+  {path: 'charts', children:[{path: 'flot', component: FlotComponent}]}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ChartsRoutingModule { }
