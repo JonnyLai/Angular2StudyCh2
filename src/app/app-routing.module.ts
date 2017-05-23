b3e347ebb1514b3df4ced19cc54eac7ef8c337ce
JonnyLai <m10573727@mail.nknu.edu.tw>
2017-05-23 16:56:12 +0800

    canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import {DashboardComponent} from 'app/dashboard/dashboard.component';
 import {fallbackRoute} from 'app/shared/fallback-rout';
 import { LoginComponent } from "app/login/login.component";
 import { LayoutComponent } from "app/layout/layout.component";
+import { LoginGuard } from "app/login.guard";
 
 const routes: Routes = [
   {path:'', component: LayoutComponent , children:[
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent, data: {title: 'Page 1'}},
-  {path: 'cards/:type', component: CardsComponent},
+  {path: 'cards/:type', component: CardsComponent , canActivate:[
+    LoginGuard
+  ]},
   {path: 'charts', loadChildren:'./charts/charts.module#ChartsModule'}]},
    {path:'login' , component: LoginComponent},
    fallbackRoute
