60c3a257832b26fc2d3fcfe3872c23348177d856
JonnyLai <m10573727@mail.nknu.edu.tw>
2017-05-23 13:46:10 +0800

    加入新的Flot Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {Route, RouterModule, Routes} from '@angular/router';
 import {CardsComponent} from 'app/cards/cards.component';
 import {DashboardComponent} from 'app/dashboard/dashboard.component';
 import {fallbackRoute} from 'app/shared/fallback-rout';
+import { FlotComponent } from "app/charts/flot/flot.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent, data: {title: 'Page 1'}},
-  {path: 'cards', component: CardsComponent}, fallbackRoute
+  {path: 'cards', component: CardsComponent},
+  {path: 'charts', children:[{path: 'flot', component: FlotComponent}]}, fallbackRoute
 ];
 
 @NgModule({
