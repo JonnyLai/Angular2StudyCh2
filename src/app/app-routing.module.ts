13049b201a03a46f9b96f31da0d6560895c821c6
JonnyLai <m10573727@mail.nknu.edu.tw>
2017-05-23 16:47:54 +0800

    Create Layout and Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import {PreloadAllModules, Route,  RouterModule,  Routes} from '@angular/router'
 import {CardsComponent} from 'app/cards/cards.component';
 import {DashboardComponent} from 'app/dashboard/dashboard.component';
 import {fallbackRoute} from 'app/shared/fallback-rout';
+import { LoginComponent } from "app/login/login.component";
+import { LayoutComponent } from "app/layout/layout.component";
 
 const routes: Routes = [
+  {path:'', component: LayoutComponent , children:[
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent, data: {title: 'Page 1'}},
   {path: 'cards/:type', component: CardsComponent},
-  {path: 'charts', loadChildren:'./charts/charts.module#ChartsModule'},
+  {path: 'charts', loadChildren:'./charts/charts.module#ChartsModule'}]},
+   {path:'login' , component: LoginComponent},
    fallbackRoute
 ];
 
