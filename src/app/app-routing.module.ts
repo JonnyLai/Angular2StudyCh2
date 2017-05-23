e7ee12d19aae73fcb7148749246c2c0de7ed0bfe
JonnyLai <m10573727@mail.nknu.edu.tw>
2017-05-23 10:32:14 +0800

    redirectTo setting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Page1Component } from 'app/page1/page1.component';
 import { Page2Component } from 'app/page2/page2.component';
 
 const routes: Routes = [
+  {path:'' , redirectTo:'/page1' , pathMatch: 'full'},
   {
     path: 'page1', component: Page1Component
   },
     {
     path: 'page2', component: Page2Component
-  }
+  },
+  {path:'**' , redirectTo:'/' , pathMatch: 'full'},
 ];
 
 @NgModule({
